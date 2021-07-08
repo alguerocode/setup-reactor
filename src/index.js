aa3fa6a3990cb1e16870d3b7e0e20560d0d2007e
salah <alhashmis246@gmail.com>
2021-07-08 14:03:26 +0400

    start with webpack react js and init files

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// require('file-loader')
+// import React from 'react';
+import "./index.css";
+import App from './App';
+// import ReactDOM from 'react-dom';
+console.log('welcome there');
+App();
