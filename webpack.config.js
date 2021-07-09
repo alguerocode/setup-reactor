2d5ce89231e3052e884e7b55f4cda383f96d5163
salah <alhashmis246@gmail.com>
2021-07-09 22:21:14 +0400

    add some comments to webpack config and edit package.json

--- webpack.config.js
+++ webpack.config.js
@@ -6,26 +6,32 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 
 
-module.exports = (env) => {
-  console.log(env)
-  console.log(process.env.NODE_ENV);
+module.exports = (env) => { // webpack function with env pramater and return webpack config ;
+                            //
+  console.log(env)          // console the webpack env object {} ;
+
   return {
-    mode: env.production ? 'production' : 'development',
-    entry: './src/index.js',
+    mode: env.production ? 'production' : 'development',//Providing the mode configuration option tells webpack
+                                                        //to use its built-in optimizations accordingly. (production | development);
+    entry: './src/index.js',                            //The entry object is where webpack looks to start building the bundle;
     output: {
-      clean: true,
-      filename: env.production ? "assets/js/[name].[fullhash].js" : "assets/js/[name].bundle.js",
-      path: path.resolve(__dirname, 'build'),
-      publicPath: './'
+      clean: true, // Clean the output directory before emit.
+      filename: env.production ? "assets/js/[name].[fullhash].js" : //This option determines the name of each output bundle;
+                                 "assets/js/[name].bundle.js",      // it determines depend on webpack env property;
+      path: path.resolve(__dirname, 'build'),                       // tells webpack where path to output the files;
+      publicPath: './'  // This option specifies the public URL of the output directory when referenced in a browser;
     },
-    devtool: env.development && "eval-cheap-source-map",
-    optimization:{
-      minimize: env.production,
-      minimizer: [
-        new TerserWebpackPlugin({
-          terserOptions: {
-            compress: {
-              comparisons:false
+    devtool: env.development && "eval-cheap-source-map",   // This option controls if and how source maps are generated if
+                                                           // webpack env.production = true is set to false for optimization and minifying the files
+    optimization:{                  // optimization section 
+      minimize: env.production,     // Tell webpack to minimize the bundle using the TerserPlugin or the plugin(s) specified in minimizer
+                                    // if webpack env.production = false don't use it and use default webpack optimization 
+      minimizer: [                  // Allows you to override the default minimizer by providing a different one or more customized
+        new TerserWebpackPlugin({   // tarser => use for minimize and optimize js files 
+          parallel:true,            // Use multi-process parallel running to improve the build speed
+          terserOptions: {          // Terser minify options 
+            compress: {              
+              comparisons:false,    
             },
             mangle: {
               safari10: true
@@ -34,7 +40,7 @@ module.exports = (env) => {
               comments: false,
               ascii_only: true
             },
-            warnings: false
+            warnings: false       // allow show warning 
           },
         }),
         new CssMinimizerPlugin()
@@ -113,10 +119,6 @@ module.exports = (env) => {
               name: "images/[name].[fullhash].[ext]",
             }
           }
-        },
-        {
-          test: /\.svg$/,
-          use: ["@svgr/webpack"]
         },
         {
           test: /\.(eot|otf|ttf|woff|woff2)$/,
