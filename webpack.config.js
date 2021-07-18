bcfa0d4df5761958ab7247d537fe11ea777377bd
salah alhashmi <alhashmis246@gmail.com>
2021-07-18 02:15:11 -0700

    remove the production only in server worker plugin and add addtional configs

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
-const WorkBoxPlugin = require('workbox-webpack-plugin');
+const {GenerateSW, InjectManifest} = require('workbox-webpack-plugin');
 {/*
   webpack configuration structure:
   mode, 
@@ -125,13 +125,14 @@ module.exports = (env) => { // webpack function with env pramater and return web
         "process.env.NODE_ENV": JSON.stringify(
           env.production ? "production" : "development"
         )
-      }),
-      env.production &&                          // the server worker will be userd in production
-      new WorkBoxPlugin.GenerateSW({             // Service workers enable advanced optimization techniques and improvements to user experience
+      }),                        
+      new GenerateSW({                           // Service workers enable advanced optimization techniques and improvements to user experience
         swDest: "service-worker.js",             // specifies the output filename for the generated worker file.
         mode: env.production ? "production" : "development",// => mode env for WorkboxPlugin
         clientsClaim: true,                      // instructs the service worker to take control of the page immediately after registration and begin serving cached resources
-        skipWaiting: true                        // makes updates to the service worker take effect immediately
+        skipWaiting: true,                       // makes updates to the service worker take effect immediately,
+        sourcemap:env.development,               // create source map for server worker in development mode
+        directoryIndex:"index.html"              // If a navigation request for a URL ending in / fails to match a precached URL, this value will be appended to the URL and that will be checked for a precache match.
       })
     ],
     module: {                     // loaders section in module.rules
