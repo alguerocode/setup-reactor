2b31272f27e50d4947073bad62e9025bf91c3dfa
salah alhashmi <alhashmis246@gmail.com>
2021-07-27 08:57:02 -0700

    add strict mode

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React from "react";
 import "./styles/index.css";
-import App from './App';
-import ReactDOM from 'react-dom';
+import App from "./App";
+import ReactDOM from "react-dom";
 
 // render to the dom
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
